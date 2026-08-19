8ba8ec6464ed43d6d31c5f08067b90763140a25b
Ahmed Shahi <ahmedshahi038@gmail.com>
2026-08-20 01:53:41 +0500

    deploy backend-2

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,13 @@ const auth=require('./routes/auth');
 const cors=require('cors');
 const app=express();
 app.use(express.json())//extract data from body
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 app.use('/auth',auth)//middleware=>route auth
 app.use("/health",(req,res)=>{
     res.json({status:"OK"})
 })
 app.listen(4000,()=>{
     console.log("server is running...");
-    
 })
